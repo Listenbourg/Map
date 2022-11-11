2f1e18b39b50460f69f45afaf846959b45aeba99
nonozgytb <nonozg.ytb@gmail.com>
2022-11-11 01:45:19 +0100

    Change 100% Style + JS Change to Outil 1 Layout Use (a fork) of svg-pan-zoom for the project
    TODO : Depart. name in the middle if zoom > n
    TODO 2 : Add cities

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import svgPanZoom from "./svg-pan-zoom/svg-pan-zoom";
+import Tooltip from "./tooltip";
+
+window.addEventListener("load", async () => {
+	const SVG_OBJECT = document.querySelector(
+		"#listenmap-svg"
+	) as HTMLObjectElement;
+	const resizeHeight = () =>
+		(SVG_OBJECT.style.height =
+			document.body.clientHeight -
+			(document.querySelector("header")?.scrollHeight || 0) -
+			1 -
+			(document.querySelector("footer")?.scrollHeight || 0) -
+			1 +
+			"px");
+
+	await sleep(150);
+
+	const SVG_ELEM = SVG_OBJECT.contentDocument
+		?.documentElement as unknown as SVGSVGElement;
+	await sleep(50);
+	const { setTooltip, hideTooltip } = Tooltip(
+		SVG_OBJECT,
+		document.querySelector("#tooltip") as HTMLDivElement
+	);
+	const map = svgPanZoom(SVG_OBJECT, {
+		zoomEnabled: true,
+		controlIconsEnabled: false,
+		preventMouseEventsDefault: false,
+		fit: 1,
+		center: 1,
+		zoomScaleSensitivity: 0.3,
+		minZoom: 0.75,
+		maxZoom: 6,
+	});
+	resizeHeight();
+	SVG_ELEM.addEventListener("mousemove", (e: any) => {
+		const region = (e.path as SVGElement[]).find((el) =>
+			el?.classList?.contains("region")
+		);
+		const regionName = region?.getAttribute("data-region-name") || "";
+		const depart = (e.path as SVGElement[]).find((el) =>
+			el?.classList?.contains("depart")
+		);
+		const departName = depart?.getAttribute("data-depart-name") || "";
+		region
+			? setTooltip(e, `${depart?.id} ${departName} (${regionName})`)
+			: hideTooltip();
+	});
+	resizeHeight();
+	map.resize();
+	map.fit();
+	map.center();
+	window.addEventListener("resize", () => {
+		resizeHeight();
+		map.resize();
+		map.fit();
+		map.center();
+	});
+});
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
--- src/tooltip.ts
+++ src/tooltip.ts
@@ -1,24 +1,25 @@
 export default function Tooltip(svg: HTMLObjectElement, element: HTMLElement) {
-  function setTooltip(e: MouseEvent, content: string) {
-    element.innerHTML = content;
-    element.style.display = "block";
+	element.addEventListener("mouseover", () => (element.style.display = "none"));
+	function setTooltip(e: MouseEvent, content: string) {
+		element.innerHTML = content;
+		element.style.display = "block";
 
-    element.style.left = (e.pageX + 10) + "px";  
+		element.style.left = e.pageX + 10 + "px";
 
-    if (
-      element.getBoundingClientRect().right >
-      svg.getBoundingClientRect().right
-    ) {
-      element.style.left = (e.pageX - element.clientWidth) + "px";
-      element.style.top = (e.pageY - element.clientHeight - 10)  + "px";
-    } else {
-      element.style.top = (e.pageY - (element.clientHeight / 2)) + "px";
-    }
-  }
+		if (
+			element.getBoundingClientRect().right > svg.getBoundingClientRect().right
+		) {
+			element.style.left = e.pageX - element.clientWidth + "px";
+			element.style.top = e.pageY - element.clientHeight - 10 + "px";
+		} else {
+			element.style.top = e.pageY - element.clientHeight / 2 + "px";
+		}
+	}
 
-  function hideTooltip() {
-    element.style.display = "none";
-  }
+	function hideTooltip() {
+		element.style.display = "none";
+		element.innerHTML = "";
+	}
 
-  return { setTooltip, hideTooltip };
+	return { setTooltip, hideTooltip };
 }
