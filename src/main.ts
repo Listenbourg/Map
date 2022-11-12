6233e389d4e66bf36cf1bcbefa1fb431335e1e40
1z2 <caemurdev@gmail.com>
2022-11-12 23:39:56 +0100

    New design + SCSS rewrite

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Tooltip from './tooltip';
-import { lighten } from './color';
 import {
   regionData,
   RegionName,
@@ -9,30 +8,28 @@ import {
   getRegionByDepartment,
 } from './regions';
 
-import './assets/main.css';
+import './assets/main.scss';
 import map from './assets/map_monochrome.svg';
 
 const app = document.getElementById('app') as HTMLDivElement;
 app.innerHTML = /* html */ `
   <header class="header">
-    <div class="header-logo">
-      <img src="assets/logo-light.svg" alt="Logo ListenMap" />
-    </div>
-    <div class="header-buttons">
-      <a href="https://github.com/Listenbourg/Map" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path fill="currentColor"
-                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
-          ></path>
-        </svg>
-      </a>
-    </div>
+    <div class="header-logo"></div>
+    <nav>
+				<a href="https://listenbourg.org" class="nav-btn important">
+          listenbourg.org
+        </a>
+    </nav>
   </header>
-  <div class="svg-wrapper">
-    <div class="region-name"></div>
+  <main class="main">
+    <div class="tooltip"></div>
     <object data="${map}" type="image/svg+xml"></object>
-  </div>
-  <div class="credits">Tagada - 1z2 - NonozgYtb</div>
+  </main>
+  <footer class="footer">
+    © Listenbourg - 2022 -
+    <a href="https://twitter.com/DevListenbourg">Twitter</a> -
+    <a href="https://github.com/Listenbourg/Map">Github</a>
+  </footer>
 `;
 
 window.addEventListener('load', () => {
@@ -40,7 +37,7 @@ window.addEventListener('load', () => {
   const svgEl = svgObject.querySelector('svg')!;
   const { setTooltip, hideTooltip } = Tooltip(
     document.querySelector('object')!,
-    app.querySelector('.region-name')!
+    app.querySelector('.tooltip')!
   );
   const groups = svgObject.querySelectorAll('g');
   const svgDepTexts: SVGTextElement[] = [];
@@ -92,7 +89,7 @@ window.addEventListener('load', () => {
         svgEl.style.setProperty('--text-display', 'none');
       }
 
-      svgEl.style.setProperty('--text-fz', `${Math.min(1, (1 / zoom) * 2)}rem`);
+      svgEl.style.setProperty('--text-fz', `${Math.min(.8, (1 / zoom) * 2)}rem`);
 
       e.preventDefault();
     },
@@ -136,6 +133,9 @@ window.addEventListener('load', () => {
     const regionName = getRegionByDepartment(groupID)!;
     const regionColor = getRegionColor(regionName as RegionName);
 
+    path.style.setProperty('--region-color-light', regionColor[0]);
+    path.style.setProperty('--region-color-dark', regionColor[1]);
+
     const textEl = svgObject.createElementNS(
       'http://www.w3.org/2000/svg',
       'text'
@@ -155,9 +155,6 @@ window.addEventListener('load', () => {
     path.style.transition = 'fill 0.25s ease';
 
     group.addEventListener('mouseenter', () => {
-      for (const regPath of regionsPaths[regionName])
-        regPath.style.fill = regionColor;
-      path.style.fill = lighten(regionColor);
       hovered = {
         region: regionName as RegionName,
         depID: groupID,
@@ -166,25 +163,7 @@ window.addEventListener('load', () => {
     });
 
     group.addEventListener('mouseleave', () => {
-      for (const path of regionsPaths[regionName])
-        path.style.fill = '';
+      for (const path of regionsPaths[regionName]) path.style.fill = '';
     });
   });
 });
-
-// Ressources :
-// https://www.petercollingridge.co.uk/tutorials/svg/interactive/javascript/
-
-// Gérer différents espaces : régions / départements ✓
-
-// Gérer les infos liées aux régions <====
-
-// Gérer l'affichage d'un point pour les villes
-
-// Gérer le fait de sélectionner une ville
-
-// Afficher les infos d'une ville
-
-// Afficher les routes principales (réseau routier autoroutes, train)
-
-// Afficher des lieux spéciaux (aéroport...)
